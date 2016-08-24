bb221c9d567e5f8b630b55f9bc4096c5ca00a80d
jcaceli <jcaceli@e-dswd.net>
2016-08-24 11:10:42 +0800

    Update user.php

--- hr/user.php
+++ hr/user.php
@@ -274,6 +274,15 @@ tbody tr {
 </head>
 <body>
 <?php require "nav.php"; ?>
+<div class="loader vcenter" style="display:none;" id="loadoverlay">
+    <div class="spinner" style="margin-top:-2em;text-align:center">
+      <h3 style="font-weight:normal;display:block">Uploading..</h3><br>
+      <div class="bounce1"></div>
+      <div class="bounce2"></div>
+      <div class="bounce3"></div>
+    </div>
+
+</div>
 <div class="row" style="margin:0;padding:0;margin-left:1em;margin-right:1em;border:solid 1px #c5d6de;background:#fff;text-align:center;padding:0">
         <div class="col-md-3 wellz" style="padding:1em">
 
